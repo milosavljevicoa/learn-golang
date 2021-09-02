2a0e5bb97bd6219066323bbbdca409f6b55af0f9
Aleksandar Milosavljevic <milosavljevic246@gmail.com>
2021-09-02 22:35:41 +0200

    wip: longest substring without repeating characters
    find the longest substring without any repeating characetrs, currently not yet optimazied
    
    ref: https://leetcode.com/problems/longest-substring-without-repeating-characters/

--- leetcode-medium/main.go
+++ leetcode-medium/main.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
 
 //https://leetcode.com/problems/add-two-numbers/
 type ListNode struct {
@@ -29,6 +32,7 @@ func addTwoNumbers(l1 *ListNode, l2 *ListNode) *ListNode {
 			appendTo.Next = newNode
 			appendTo = newNode
 		}
+
 	}
 
 	if currList1 != nil {
@@ -70,7 +74,32 @@ func runAddTwoNumbers() {
 	printList(addTwoNumbers(&l1, &l2))
 }
 
+//https://leetcode.com/problems/longest-substring-without-repeating-characters/
+func lengthOfLongestSubstring(s string) int {
+	maxSubStringLen := 0
+	currSubStringLen := 0
+	substring := ""
+
+	for i := 0; i < len(s)-1; i++ {
+		currSubStringLen = 0
+		for j := i + 1; j < len(s); j++ {
+			if strings.Contains(substring, string(s[j])) {
+				if maxSubStringLen < currSubStringLen {
+					maxSubStringLen = currSubStringLen
+					break
+				}
+			} else {
+				substring += string(s[j])
+				currSubStringLen++
+			}
+		}
+		substring = ""
+	}
+
+	return maxSubStringLen
+}
+
 func main() {
-	fmt.Println("vim-go")
+	fmt.Println(lengthOfLongestSubstring("0"))
 	//runAddTwoNumbers()
 }
